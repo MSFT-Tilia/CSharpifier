236b01d6b6cec91fe83d14303586d3dd5489daf8
MSFT-Tilia <tilia@microsoft.com>
2020-12-24 12:50:39 +0800

    interpret namespace and simple-declaration

--- CSharpifier/CSharpifier/CSharpConverter.cs
+++ CSharpifier/CSharpifier/CSharpConverter.cs
@@ -24,19 +24,56 @@ namespace CSharpifier
             }
         }
 
+        #region visitors
+
+        // Using Directive
         public override void EnterUsingDirective([NotNull] CPPCXParser.UsingDirectiveContext context)
         {
             LineAppendTerm("using");
-
             LineAppendTerm(InterpretDoubleColon(context.attributeSpecifierSeq()?.GetText()));
             LineAppendTerm(InterpretDoubleColon(context.nestedNameSpecifier()?.GetText()), context.attributeSpecifierSeq() != null);
             LineAppendTerm(InterpretDoubleColon(context.namespaceName()?.GetText()), context.nestedNameSpecifier() != null);
-            LineAppendSemi();
+        }
 
+        public override void ExitUsingDirective([NotNull] CPPCXParser.UsingDirectiveContext context)
+        {
+            LineAppendSemi();
             ResultsAppendLine();
         }
 
-        private string InterpretDoubleColon(string origin)
+        // Namespace Definition
+        public override void EnterNamespaceDefinition([NotNull] CPPCXParser.NamespaceDefinitionContext context)
+        {
+            LineAppendTerm("namespace");
+            LineAppendTerm(InterpretDoubleColon(context.qualifiednamespacespecifier()?.GetText()));
+            ResultsAppendLine();
+
+            LineAppendLeftBrace();
+            ResultsAppendLine();
+        }
+
+        public override void ExitNamespaceDefinition([NotNull] CPPCXParser.NamespaceDefinitionContext context)
+        {
+            LineAppendRightBrace();
+            ResultsAppendLine();
+        }
+
+
+        public override void EnterSimpleDeclaration([NotNull] CPPCXParser.SimpleDeclarationContext context)
+        {
+            LineAppendTerm(InterpretDoubleColon(context.attributeSpecifierSeq()?.GetText()));
+            LineAppendTerm(InterpretDoubleColon(context.declSpecifierSeq()?.GetText()));
+            LineAppendTerm(context.initDeclaratorList()?.GetText());
+            ResultsAppendLine();
+        }
+
+
+
+        #endregion // visitors
+
+        #region string format helpers
+
+        private static string InterpretDoubleColon(string origin)
         {
             if(!string.IsNullOrEmpty(origin))
             {
@@ -63,6 +100,16 @@ namespace CSharpifier
             }
         }
 
+        private void LineAppendLeftBrace()
+        {
+            LineAppendTerm("{");
+        }
+
+        private void LineAppendRightBrace()
+        {
+            LineAppendTerm("}");
+        }
+
         private void LineAppendSemi()
         {
             LineAppendTermNoSpace(";");
@@ -82,7 +129,11 @@ namespace CSharpifier
             LineClear();
         }
 
+        #endregion // string format helpers
+
+        #region private members
         private StringBuilder _line;
         private StringBuilder _results;
+        #endregion // private members
     }
 }
